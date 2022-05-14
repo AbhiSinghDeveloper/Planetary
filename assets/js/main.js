84934e633af183640f279f0aac111b3e1568d8bb
shivamksinha11 <shivamksinha11@gmail.com>
2022-05-14 16:59:35 +0530

    carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,6 +63,14 @@
   /**
    * Scrolls to an element with header offset
    */
+   let heroCarouselIndicators = select("#hero-carousel-indicators")
+   let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+ 
+   heroCarouselItems.forEach((item, index) => {
+     (index === 0) ?
+     heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+       heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+   });
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
